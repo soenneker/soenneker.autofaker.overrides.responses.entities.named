8aa67cbde2d8545098913e119c36da4b48cd19e8
Jake Soenneker <4441470+soenneker@users.noreply.github.com>
2026-04-21 19:01:14 -0500

    TUnit conversion

--- test/Soenneker.AutoFaker.Overrides.Responses.Entities.Named.Tests/NamedEntityResponseTests.cs
+++ test/Soenneker.AutoFaker.Overrides.Responses.Entities.Named.Tests/NamedEntityResponseTests.cs
@@ -1,11 +1,10 @@
-﻿using Soenneker.Tests.Unit;
-using Xunit;
+using Soenneker.Tests.Unit;
 
 namespace Soenneker.AutoFaker.Overrides.Responses.Entities.Named.Tests;
 
 public sealed class NamedEntityResponseTests : UnitTest
 {
-    [Fact]
+    [Test]
     public void Default()
     {
 
