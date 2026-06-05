3914d113c7f81be6a5ffd8da5a7e2cea9a7ed680
Jake Soenneker <4441470+soenneker@users.noreply.github.com>
2026-06-04 19:35:54 -0500

    xml commenting

--- src/Soenneker.AutoFaker.Overrides.Responses.Entities.Named/NamedEntityResponseOverride.cs
+++ src/Soenneker.AutoFaker.Overrides.Responses.Entities.Named/NamedEntityResponseOverride.cs
@@ -9,11 +9,20 @@ namespace Soenneker.AutoFaker.Overrides.Responses.Entities.Named;
 /// </summary>
 public class NamedEntityResponseOverride : AutoFakerOverride<NamedEntityResponse>
 {
+    /// <summary>
+    /// Executes the can override operation.
+    /// </summary>
+    /// <param name="context">The context.</param>
+    /// <returns>A value indicating whether the operation succeeded.</returns>
     public override bool CanOverride(AutoFakerContext context)
     {
         return context.GenerateType.IsSubclassOf(typeof(NamedEntityResponse));
     }
 
+    /// <summary>
+    /// Executes the generate operation.
+    /// </summary>
+    /// <param name="context">The context.</param>
     public override void Generate(AutoFakerOverrideContext context)
     {
     }
